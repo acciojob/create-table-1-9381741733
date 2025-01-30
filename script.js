b977d1586ea85d2adc209c7892db7a6adfd22ef2
jyothsna bhukya <jyothsnabhukya022@gmail.com>
2025-01-30 21:22:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function insert_Row() {
-    //Write your code here
-  
-  
+
+	function insert_Row() {
+  const table = document.getElementById('sampleTable');
+
+  // Create a new row at the top (index 0)
+  const newRow = table.insertRow(1); 
+
+  // Insert cells in the new row
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+
+  // Assign values to the cells
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
 }
+
+  
+  
+
